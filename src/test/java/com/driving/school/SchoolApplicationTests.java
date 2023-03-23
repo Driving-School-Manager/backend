2cab2947d2a6db79d72f34be4317e6c7a3a220aa
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-03-23 19:35:36 +0100

    initial commit

---
+++ src/test/java/com/driving/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driving.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
