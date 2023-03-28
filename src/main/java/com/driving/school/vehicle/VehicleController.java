c31c4fc37cd26bb9dd3c15d2b9bd05831ca7395e
Jakub Piecuch <kuba.piecuch.kp@gmail.com>
2023-03-28 20:07:50 +0200

    added data.sql to .gitignore file

--- src/main/java/com/driving/school/vehicle/VehicleController.java
+++ src/main/java/com/driving/school/vehicle/VehicleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/vehicles")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO vehicle VALUES ('1', 'Bmw', 'active', 'manual', '2006');
+INSERT INTO vehicle VALUES ('2', 'Toyota', 'active', 'manual', '2022');
+INSERT INTO vehicle VALUES ('3', 'Audi', 'active', 'automatic', '2015');
+INSERT INTO vehicle VALUES ('4', 'Seat', 'active', 'automatic', '2010');
+INSERT INTO vehicle VALUES ('5', 'Fiat', 'active', 'manual', '2012');
